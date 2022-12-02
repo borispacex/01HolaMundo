3519b74a58c3f1d7649b72997901e7958d736ddc
Boris Vargas <borisvargaspaucara@gmail.com>
2022-12-02 06:37:54 -0400

    subiendo cambios

--- src/app/components/register-user/register-user.component.ts
+++ src/app/components/register-user/register-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, Validators, FormBuilder } from "@angular/forms";
+import { FormGroup, Validators, FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-register-user',
@@ -8,10 +8,43 @@ import { FormGroup, Validators, FormBuilder } from "@angular/forms";
 })
 export class RegisterUserComponent implements OnInit  {
 
-  constructor(private formBuilder: FormBuilder) {}
-
-  ngOnInit() {
+  registroUserForm!: FormGroup;
+  
+  constructor() { }
+  ngOnInit(): void {
+    this.registroUserForm = new FormGroup({
+      nombres: new FormControl(''),
+      apellidos: new FormControl(''),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      confirmar: new FormControl('', [Validators.required])
+    });
   }
-
+  public validateControl (controlName: string) {
+    return this.registroUserForm.get(controlName)?.invalid && this.registroUserForm.get(controlName)?.touched;
+  }
+  public hasError (controlName: string, errorName: string) {
+    return this.registroUserForm.get(controlName)?.hasError(errorName);
+  }
+  
+  public registrarUser (registerFormValue: any) {
+    const formValues = { ...registerFormValue };
+    const user: UsuarioDto = {
+      nombres: formValues.nombres,
+      apellidos: formValues.apellidos,
+      email: formValues.email,
+      password: formValues.password,
+      confirmarPassword: formValues.confirmar
+    };
+    console.log(user);
+  }
  
+}
+
+interface UsuarioDto {
+  nombres: string;
+  apellidos: string;
+  email: string;
+  password: string;
+  confirmarPassword: string;
 }
