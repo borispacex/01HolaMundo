e4ab6b9c79ad9798a7c363598877bd82c2ab8f80
Boris Vargas <borisvargaspaucara@gmail.com>
2022-12-05 02:38:38 -0400

    Directivas

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  
+  // ngClass
+  fondoAmarillo = false;
+  letraGrande = false;
 
+  cambiarFondo(): void {
+    this.fondoAmarillo = !this.fondoAmarillo;
+  }
+  cambiarLetra(): void {
+    this.letraGrande = !this.letraGrande;
+  }
+  asignaClases(): object {
+    let classes = {
+      fa: this.fondoAmarillo,
+      lg: this.letraGrande,
+    };
+    return classes;
+  }
+  // ngSwitch
+  variable = 'C';
+  // ngModel
+  nombre = '';
+  cambiaNombre($event: any): void {
+    this.nombre = this.nombre.toUpperCase();
+  }
+  // ngStyle
+  canSave = true;
 }
