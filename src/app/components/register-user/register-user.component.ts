780c2a939a18a00e0930e638fff94f3d0326fc6f
Boris Vargas <borisvargaspaucara@gmail.com>
2022-12-07 07:04:35 -0400

    commit formulario reactivo

--- src/app/components/register-user/register-user.component.ts
+++ src/app/components/register-user/register-user.component.ts
@@ -13,11 +13,11 @@ export class RegisterUserComponent implements OnInit  {
   constructor() { }
   ngOnInit(): void {
     this.registroUserForm = new FormGroup({
-      nombres: new FormControl(''),
-      apellidos: new FormControl(''),
+      nombres: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(10)]),
+      apellidos: new FormControl('', [Validators.required, Validators.pattern(/^[A-Za-z\s\xF1\xD1]+$/)]),
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-      confirmarPassword: new FormControl('', [Validators.required, Validators.maxLength(10)])
+      password: new FormControl('', [Validators.required]),
+      confirmarPassword: new FormControl('', [Validators.required])
     });
   }
   public validateControl (controlName: string) {
